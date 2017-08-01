e562502b04e506a08148bd7dbc3dc6c05d59f28d
mateuswetah <mateus.m.luna@gmail.com>
2017-08-01 09:28:04 -0300

    Adiciona diretiva de esconder e mostrar Header de acordo com o Scroll na página de Posts.

--- src/directives/shrink-header/shrink-header.ts
+++ src/directives/shrink-header/shrink-header.ts
@@ -11,32 +11,37 @@ export class ShrinkHeader {
   header: any;
   headerHeight: any;
   translateAmt: any;
-   
+  previousTop: number = 0;
+
   constructor(public element: ElementRef, public renderer: Renderer) {  }
  
   ngAfterViewInit() {      
     this.header = document.getElementById("shrinkHeader");
     this.headerHeight = this.header.clientHeight;
-
+    this.renderer.setElementStyle(this.header, 'transition', 'transform .2s ease-in');
   }
   
   onContentScroll(ev) {
-     ev.domWrite(() => {
-            this.updateHeader(ev);
-        });   
+    ev.domWrite(() => {
+      this.updateHeader(ev);
+    });   
   }
  
   updateHeader(ev) {
- 
-    if (ev.scrollTop >= 0) {
-      this.translateAmt = -ev.scrollTop / 4;
-       
+
+    if (this.previousTop > ev.scrollTop) {  
+      if (this.header.style['webkitTransform'] != 'translate3d(0px, 0px, 0px)') {
+        this.renderer.setElementStyle(this.header, 'webkitTransform', 'translate3d(0,' + 0 + 'px,0)');
+      }
     } else {
-       this.translateAmt =ev.scrollTop / 4;
+      if (ev.scrollTop > 0) {
+        this.translateAmt = -ev.scrollTop / 2;    
+        this.renderer.setElementStyle(this.header, 'webkitTransform', 'translate3d(0,' + this.translateAmt + 'px,0)');
+      }  
     }
- 
-    this.renderer.setElementStyle(this.header, 'webkitTransform', 'translate3d(0,' + this.translateAmt + 'px,0)');
- 
+    
+    this.previousTop = ev.scrollTop;
+
   }
  
  
